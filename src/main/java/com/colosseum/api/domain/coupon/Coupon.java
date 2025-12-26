110d55856daf06e70438d17a3f7cd62929046387
João Victor <joaovictorpinheiro1510@gmail.com>
2025-12-26 12:21:55 -0300

    Add domains

---
+++ src/main/java/com/colosseum/api/domain/coupon/Coupon.java
@@ -0,0 +1,37 @@
+package com.colosseum.api.domain.coupon;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.colosseum.api.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "coupon")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
